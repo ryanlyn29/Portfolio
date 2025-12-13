d4c39e05339777c629acc11d50fb55f9bb3e218e
Ryan Lyncee <ryanlyncee29@gmail.com>
2025-12-12 21:01:38 -0500

    modified deployment error with e declaration

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -822,7 +822,7 @@ export default function App() {
                 drag="x"
                 dragConstraints={{ left: 0, right: 0 }}
                 dragElastic={1}
-                onDragEnd={(e, { offset, velocity }) => {
+                onDragEnd={(_e, { offset, velocity }) => {
                     const swipe = swipePower(offset.x, velocity.x);
 
                     if (swipe < -swipeConfidenceThreshold) {
